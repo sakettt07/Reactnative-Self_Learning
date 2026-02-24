6efd0f1379dff38903fb8962c954b6d78e125bbb
saket <guptasaket898@gmail.com>
2026-02-24 23:57:56 +0530

    Learning hooks useRef,useEffect how to use them more efficiently

--- src/navigation/bottom-tabs/screens/Home.tsx
+++ src/navigation/bottom-tabs/screens/Home.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Dimensions, TextInput, Button } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Home = () => {
+    const [orientation, setOrientation] = useState('Portrait');
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        const handleOrientationChange = () => {
+            const { width, height } = Dimensions.get('window');
+            setOrientation(width > height ? 'Landscape' : 'Portrait');
+        }
+        const subscription = Dimensions.addEventListener(
+            'change', handleOrientationChange
+        );
+        return () => {
+            subscription.remove();  // necessary to avoid memory leak
+        }
+    }, [])
     return (
         <View>
             <Text>Understanding useState</Text>
+            <TextInput ref={inputRef} style={{
+                width: '90%', height: 50, borderWidth: 1, marginTop: 20
+            }} />
+            <Button onPress={() => {
+                inputRef.current.focus();
+            }} title='Focus Input' />
         </View>
     )
 }
