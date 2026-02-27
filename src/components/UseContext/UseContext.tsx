6db0765ccddb2baada62cf31719ebdc67d7863d5
saket <guptasaket898@gmail.com>
2026-02-27 23:59:35 +0530

    Update UseContext component to remove placeholder text and add context tutorial comment

--- src/components/UseContext/UseContext.tsx
+++ src/components/UseContext/UseContext.tsx
@@ -2,14 +2,14 @@ import { View, Text } from 'react-native'
 import React, { createContext } from 'react';
 
 export const UserContext = createContext();
-
+// use Contect tutorial in react native 
 const UseContext = () => {
     return (
         <UserContext.Provider value={{
             name: "saket", email: "saket@gmail.com"
         }}>
             <View>
-                <Text>UseContext</Text>
+                <Text></Text>
             </View>
         </UserContext.Provider>
     )
